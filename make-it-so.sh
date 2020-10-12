b8832982ecfd5c81361057c09a086ffbc242f647
0066cc <42606248+0066cc@users.noreply.github.com>
2020-10-12 12:26:39 +0100

    Update make-it-so.sh

--- make-it-so.sh
+++ make-it-so.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo apt install vim zathura
+sudo apt install vim zathura curl
+curl -sf https://gobinaries.com/chriswalz/bit | sh;
+curl -sf https://gobinaries.com/chriswalz/bit/bitcomplete | sh && echo y | COMP_INSTALL=1 bitcomplete
