7716b63ff5f72eac16051201c818171e6d891f9f
0066cc <0066cc@users.noreply.github.com>
2020-12-20 08:43:24 +0000

    added update script

--- update-time.sh
+++ update-time.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.bashrc ~/.bashrc
-curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.tmux.conf ~/.tmux.conf
-curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.xsession ~/.xsession
-curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.vimrc ~/.vimrc
-curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.Xresources ~/.Xresources
+curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.bashrc ~/.bashrc ;
+curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.tmux.conf ~/.tmux.conf ;
+curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.xsession ~/.xsession ;
+curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.vimrc ~/.vimrc ; 
+curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/.Xresources ~/.Xresources ;
+curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/config/i3/config ~/.config/i3 ;
+curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/config/zathura/zathurarc ~/.config/zathura/ ;
+curl https://raw.githubusercontent.com/0066cc/Dotfiles/master/newsboat/config ~/.newsboat/
