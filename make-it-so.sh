0ee2bb8a628aae69e71b716a60b9130c89024fab
0066cc <0066cc@users.noreply.github.com>
2021-03-26 06:45:52 +0000

    Update

--- make-it-so.sh
+++ make-it-so.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo apt install vim zathura ncdu htop dict newsboat curl git tmux build-essential xdm
+sudo apt install vim zathura ncdu htop dict newsboat curl git tmux build-essential xdm lynx w3m nnn
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
