c295efb01cf5c34fbe6320bdfc1d3d0a0fe895d9
0066cc <0066cc@users.noreply.github.com>
2021-03-29 18:00:01 +0100

    "Automated update"

--- make-it-so.sh
+++ make-it-so.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-sudo apt install vim zathura ncdu htop dict newsboat curl git tmux build-essential xdm lynx w3m nnn acpi rxvt-unicode-256color mutt
+sudo apt install vim zathura ncdu htop dict qutebrowser newsboat curl git tmux build-essential xdm lynx w3m nnn acpi rxvt-unicode-256color mutt dunst
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+mkdir -p ~/Documents/git
+cd ~/Documents/git/
+git clone https://git.suckless.org/slock
+cd slock
+sudo make clean install
 tmux source ~/.tmux.conf
+source ~/.bashrc
