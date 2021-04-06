7849b114b08212e8c6db22794c1643fbb438c491
0066cc <0066cc@users.noreply.github.com>
2021-04-06 20:00:01 +0100

    "Automated update"

--- make-it-so.sh
+++ make-it-so.sh
@@ -2,10 +2,11 @@
 sudo apt install vim zathura ncdu htop dict qutebrowser newsboat curl git tmux build-essential xdm lynx w3m nnn acpi rxvt-unicode-256color mutt dunst
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-mkdir -p ~/Documents/git
+mkdir -p ~/Documents/git/etc
 cd ~/Documents/git/
 git clone https://git.suckless.org/slock
 cd slock
 sudo make clean install
 tmux source ~/.tmux.conf
 source ~/.bashrc
+
